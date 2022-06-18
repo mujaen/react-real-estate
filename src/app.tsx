af215f5626417d3c24696e680cbc8439453dc78e
mujaen <mujaen@nate.com>
2022-06-18 16:20:24 +0900

    feat: GlobalStyles, ThemeProvider 설정

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-
+import styled, {ThemeProvider} from 'styled-components';
+import {themeStyles} from 'style/theme';
+import GlobalStyles from 'style/global';
 const rootNode = document.getElementById('app');
+
+const StyledDiv = styled.div`
+    color: ${(props) => props.theme.colors.primary}
+`
 
 function App({}) {
     return (
-        <>
-            12
-        </>
+        <ThemeProvider theme={themeStyles}>
+            <GlobalStyles />
+            <StyledDiv>asdasdasd</StyledDiv>
+        </ThemeProvider>
     )
 }
 
---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+const colors = {
+    primary: "red",
+    secondary: "white",
+}
+
+export const themeStyles = {
+    colors,
+};
+
+export type Theme = typeof themeStyles;
+
